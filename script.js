9193737f2c5b216adc6846fc2cc4228d597909f8
Ryan Burnett <ryanpburnett412@gmail.com>
2020-11-21 13:37:17 -0600

    addTodo

--- script.js
+++ script.js
@@ -3,9 +3,12 @@ var todoForm = document.querySelector("#todo-form");
 var todoList = document.querySelector("#todo-list");
 var todoCountSpan = document.querySelector("#todo-count");
 
-var todos = ["Learn HTML", "Learn CSS", "Learn JavaScript"];
+var todos = ["Learn HTML", "Learn CSS", "Learn JavaScript", ];
 
 function renderTodos() {
+    // clear existing todos
+    todoList.innerHTML = ''
+    // re-render todos
     for (var i = 0; i < todos.length; i++) {
         // create li element
         var li = document.createElement('li')
@@ -15,5 +18,21 @@ function renderTodos() {
         todoList.appendChild(li)
     }
 }
+
+function addTodo(event) {
+    event.preventDefacult()
+    // get todoImput value
+    var newTodo = todoInput.value
+
+    if (newTodo !== '')   {
+        // push value into todos
+        todos.push(newTodo)
+        todoInput.value = null
+        renderTodos()
+        
+    }
+}
+
+todoForm.addEventListener('submit', addTodo)
 
 renderTodos()
